2c44bd673520f9658e8cea65d87f43827043f041
KwokPacific <pacific2390@gmail.com>
2025-08-21 21:10:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ class ChineseTranslator {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ body: { text: text } })
+                body: JSON.stringify({ text: text })
             });
 
             const responseText = await response.text();
